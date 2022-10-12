2d35ab360408d9a60cd47dc1851df257523de54a
iamMuhammedKaplan <kpln12345@gmail.com>
2022-10-12 15:34:12 +0300

    Field&Attribute-Encapsulation-Getter&Setter-Constructor
    yt engin demiroğ java2022 ödev-2

---
+++ Homework/Field&Attribute-Encapsulation-Getter&Setter-Constructor/src/ProductManager.java
@@ -0,0 +1,14 @@
+
+public class ProductManager {
+	
+	public void add(Product product) {
+		//With JDBC, we will handle much more later.
+		System.out.println("Ürün eklendi " + product.getName());
+	}
+	
+	public void add2(int id, String name, String description,
+					int stockAmount, double price,
+					String color) {
+		
+	}
+}
